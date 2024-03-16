d23ad428a83a8562f36aff50d6e653b1658b3d63
Uliay8 <ulanavorobeva130@gmail.com>
2024-03-16 12:07:35 +0500

    fr

--- catalog.js
+++ catalog.js
@@ -2,27 +2,30 @@ import { Catalog } from "./src/components/catalog.js"
 
 const renderPostItem = item => `
     <a  
-        href="posts/${item.id}"
+        href="posts.html?id=${item.id}"
         class="post-item"
     >
         <span class="post-item__title">
             ${item.title}
-        </span>
+        </span><br>
 
         <span class="post-item__body">
             ${item.body}
-        </span>
+        </span><br><br>
     </a>
 `
 
-const getPostItems = ({ limit, page }) => {
-    return fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`)
-        .then(async res => {
-            const total = +res.headers.get('x-total-count')
-            const items = await res.json()
-            return { items, total }
-        })
-}
+const getPostItems = async ({ limit, page }) => {
+    try {
+        const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`);
+        const total = +res.headers.get('x-total-count');
+        const items = await res.json();
+        return { items, total }
+    }
+    catch(err) {
+        console.log(err);
+    }
+};
 
 const renderPhotoItem = item => `
     <a  
@@ -40,21 +43,24 @@ const renderPhotoItem = item => `
     </a>
 `
 
-const getPhotoItems = ({ limit, page }) => {
-    return fetch(`https://jsonplaceholder.typicode.com/photos?_limit=${limit}&_page=${page}`)
-        .then(async res => {
-            const total = +res.headers.get('x-total-count')
-            const items = await res.json()
-            return { items, total }
-        })
+const getPhotoItems = async ({ limit, page }) => {
+    try {
+        const res = await fetch(`https://jsonplaceholder.typicode.com/photos?_limit=${limit}&_page=${page}`);
+        const total = +res.headers.get('x-total-count');
+        const items = await res.json();
+        return { items, total }
+    }
+    catch(err) {
+        console.log(err);
+    }
 }
 
 const init = () => {
     const catalog = document.getElementById('catalog')
-    new Catalog(catalog, { 
+    new Catalog(catalog, {
         renderItem: renderPostItem,
         getItems: getPostItems
-     }).init()
+    }).init()
 }
 
 if (document.readyState === 'loading') {
